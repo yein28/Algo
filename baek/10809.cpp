fab4e003d81325e67a21e0029338545a555714e3
yein28 <gkshs1234@gmail.com>
2017-01-25 20:56:56 +0000

    알파벳 찾기

---
+++ baek/10809.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main(){
+	char word[101];
+	int index[26];
+	fill_n( index, 26, -1 );
+	cin >> word;
+	int len = strlen(word);
+
+	for( int i = 0 ; i < len ; i++ ){
+		int c = word[i] - 'a';
+		if( index[c] == -1 ){ index[c] = i; }
+	}
+	for( int j = 0 ; j < 26 ; j++ ){
+		cout << index[j] << " ";
+	}
+	return 0;
+}
