458c5c02572aa7fbe54cfb999f2391191e280dff
yein28 <gkshs1234@gmail.com>
2017-12-28 00:42:13 +0000

    https://www.acmicpc.net/problem/11720

--- baek/11720.cpp
+++ baek/11720.cpp
@@ -10,15 +10,23 @@ int main(){
 	char buf[101];
 
 	scanf("%d", &size);
-	scanf("%s", buf);
+// 서식문자열 옵션으로 해결
+	for( int i = 0 ; i < size ; i++ ){
+		int x;
+		scanf("%1d", &x );
+		res += x;
+	}
+// 숫자를 문자로 받아서 해결 
+/*	scanf("%s", buf);
 	
 	for( int i = 0 ; i < size ; i++ ){
 		char ch = buf[i];
 		int sum = ch - '0';
 		res = res + sum;
 	}
-
+*/
 	printf("%d \n", res );
+
 // 굳이 숫자를 숫자로 받으려고해서 삽질함..
 /*	double n;
 	scanf("%d", &num);
